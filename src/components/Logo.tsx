37b07b770348dc51dc5b378661283c662fcb54d5
Luke Ponga <developmentdesignsltd@gmail.com>
2025-12-04 06:58:25 +0000

    Edit application icon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,17 +7,15 @@ export function Logo() {
           width="20"
           height="20"
           viewBox="0 0 24 24"
-          fill="currentColor"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path
-            d="M8.2,20.4H4.8V9.8C4.8,8,6.2,6.6,8,6.6H12.2V9.9H8.2V20.4z"
-            fill="currentColor"
-          />
-          <path
-            d="M19.2,9.9h-4V6.6h5.8c-0.2-2.9-2.5-5.1-5.4-5.1c-3,0-5.4,2.4-5.4,5.4v0.2h3.4v-0.2c0-1.1,0.9-2,2-2s2,0.9,2,2v0h-3.8c-1.8,0-3.2,1.4-3.2,3.2v7.3h10.8V9.9z"
-            fill="currentColor"
-          />
+          <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
+          <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
         </svg>
       </div>
       <span className="font-headline">JobTrack</span>
