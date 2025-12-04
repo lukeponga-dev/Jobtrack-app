dacaf83a08c4cb8e2dd3cbd8841896950548cf0c
Luke Ponga <developmentdesignsltd@gmail.com>
2025-12-04 12:14:20 +0000

    project clean up

--- src/lib/data.ts
+++
@@ -1,9 +0,0 @@
-import type {Application} from './types';
-import {PlaceHolderImages} from './placeholder-images';
-
-const findImage = (id: string) =>
-  PlaceHolderImages.find(img => img.id === id)?.imageUrl;
-
-export const applications: Application[] = [
-  // This data is now fetched from firestore
-];
