f98b6bcd8e8b63efc069747747bf0e2900411fa2
lukeponga-dev <developmentdesignsltd@gmail.com>
2025-12-15 13:14:49 +0000

    Change app icon image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -14,7 +14,9 @@ export function Logo() {
           strokeLinejoin="round"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path d="M9 18V5l7 4-7 4" />
+          <path d="M3 3v18h18" />
+          <path d="M7 17V7h10v10" />
+          <path d="M12 12.5v-5.5" />
         </svg>
       </div>
       <span className="font-headline">JobTrack</span>
