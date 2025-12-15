0fdc5897e30f705b5aec4ac494655002766e1ce5
lukeponga-dev <developmentdesignsltd@gmail.com>
2025-12-15 13:16:11 +0000

    Change svg icon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -14,9 +14,8 @@ export function Logo() {
           strokeLinejoin="round"
           xmlns="http://www.w3.org/2000/svg"
         >
-            <circle cx="12" cy="12" r="10" />
-            <circle cx="12" cy="12" r="6" />
-            <circle cx="12" cy="12" r="2" />
+          <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
+          <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
         </svg>
       </div>
       <span className="font-headline">JobTrack</span>
